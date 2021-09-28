bec8fa9ac8dc56f3a0baf0c4dfce2aa44a4c75a4
Harshita Gupta <47924686+HarshitaGupta16@users.noreply.github.com>
2021-09-28 20:38:07 +0530

    code refactor

--- src/index.js
+++ src/index.js
@@ -25,22 +25,19 @@ function calculateProitAndLoss(initialPrice, quantity, currentPrice) {
   if (initialPrice !== 0 && quantity !== 0 && currentPrice !== 0) {
     if (initialPrice > currentPrice) {
       // Loss
-      let lossOnTotalQuantiy = (initialPrice - currentPrice) * quantity;
-      let lossPercent = ((lossOnTotalQuantiy / initialPrice) * 100).toFixed(2);
+      let loss = initialPrice - currentPrice;
+      let lossPercent = ((loss / initialPrice) * 100).toFixed(2);
       outputMessageDisplay(
-        `The loss is ${lossOnTotalQuantiy} and loss percentage is ${lossPercent}`
+        `The loss is ${loss} and loss percentage is ${lossPercent}%`
       );
       outputResult.style.display = "block";
       outputResult.style.backgroundColor = "#DC2626";
     } else if (initialPrice < currentPrice) {
       // Profit
-      let profitOnTotalQuantity = (currentPrice - initialPrice) * quantity;
-      let profitPercent = (
-        (profitOnTotalQuantity / initialPrice) *
-        100
-      ).toFixed(2);
-      outputMessageDisplay(`The profit is ${profitOnTotalQuantity} and the profit 
-    percentage is ${profitPercent} `);
+      let profit = currentPrice - initialPrice;
+      let profitPercent = ((profit / initialPrice) * 100).toFixed(2);
+      outputMessageDisplay(`The profit is ${profit} and the profit 
+    percentage is ${profitPercent}%`);
       outputResult.style.display = "block";
       outputResult.style.backgroundColor = "#65A30D";
     } else {
